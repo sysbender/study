950e07348b449a4433d13be3224a265aa0cdb827
sysbender <sysbender@gmail.com>
2018-03-23 23:16:58 -0400

    007 poo game

--- cpp/Chili/chili_framework/Engine/Game.h
+++ cpp/Chili/chili_framework/Engine/Game.h
@@ -23,6 +23,109 @@
 #include "Keyboard.h"
 #include "Mouse.h"
 #include "Graphics.h"
+#include <random>
+
+
+struct Point
+{
+	int x;
+	int y;
+	Point(int _x, int _y): x(_x), y(_y){}
+};
+
+
+struct Rect
+{
+	int width;
+	int height;
+	Rect(int w, int h) : width(w), height(h) {}
+};
+
+struct Velocity
+{
+	int vx;
+	int vy;
+	Velocity() : vx((rand() % 2) - 1), vy((rand() % 2) - 1){}
+	//int dx = (rand() % 2) - 1;
+	//int dy = (rand() % 17) - 8;
+};
+
+struct Poo
+{
+	bool isEaten = false;
+	Point location;
+	Velocity velocity ;
+
+	Rect rect = Rect(24, 24);
+	//
+	Poo(int x, int y) : location(x, y) {}
+	//
+	// move
+	void RandomMove(Rect screen)
+	{
+
+		location.x = location.x + velocity.vx;
+		if (location.x < 0)
+		{
+			location.x = 0;
+			velocity.vx = 0 - velocity.vx;  // negate the velocity
+		}
+
+		if (location.x >= screen.width - rect.width) 
+		{
+			location.x = screen.width - 1 - rect.width;
+			velocity.vx = 0 - velocity.vx;
+		}
+
+		location.y = location.y + velocity.vy;
+		if (location.y < 0)
+		{
+			location.y = 0;
+			velocity.vy = 0 - velocity.vy;
+		}
+		if (location.y > screen.height - 1 - rect.height)
+		{
+			location.y = screen.height - 1 - rect.height;
+			velocity.vy = 0 - velocity.vy;
+		}
+	}
+
+
+};
+
+
+
+struct Dude
+{
+	Point location;
+	Rect rect;
+
+	//---------
+	Dude(int x, int y):location(x, y), rect(20,20) {}
+	
+
+	// move
+	void move( Point dest)
+	{	
+		location = dest; 
+	}
+
+	// collision detection
+	bool IsColliding(Poo poo) const
+	{
+		Point p = poo.location;
+		Rect r = poo.rect;
+		return location.x <= p.x + r.width
+			&& location.x + rect.width >= p.x
+			&& location.y <= p.y + r.height
+			&& location.y + rect.height >= p.y;
+
+	}
+ 
+
+};
+
+
 
 class Game
 {
@@ -37,10 +140,21 @@ private:
 	/********************************/
 	/*  User Functions              */
 	/********************************/
+	void DrawFace(int x, int y);
+	void DrawPoo(int x, int y);
+
+	int ClampScreenX(int x, int width);
+	int ClampScreenY(int y, int height);
+	
 private:
 	MainWindow& wnd;
 	Graphics gfx;
 	/********************************/
 	/*  User Variables              */
 	/********************************/
+
+	Dude dude = Dude(400, 300);
+	Poo poo0 = Poo(300,150);
+	Poo poo1 = Poo(100, 500);
+	Poo poo2 = Poo(600, 400);
 };
