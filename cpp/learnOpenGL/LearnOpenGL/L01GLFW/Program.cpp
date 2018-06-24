bf17d7d12e561056a7f6ac68ad65b6883856acc9
sysbender <sysbender@gmail.com>
2018-06-24 17:10:43 -0400

    learn-opengl - first window

--- cpp/learnOpenGL/LearnOpenGL/L01GLFW/Program.cpp
+++ cpp/learnOpenGL/LearnOpenGL/L01GLFW/Program.cpp
@@ -7,6 +7,20 @@
 
 #include <iostream>
 
+
+
+/*----------callback : when resizing the window-----------
+void framebuffer_size_callback(GLFWwindow * window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+
+
+*/
+
+
+}
+
 int main() {
 
 	glfwInit();
@@ -16,6 +30,8 @@ int main() {
 
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);   // for mac
 
+
+	// creat window
 	GLFWwindow * window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Fail to create GLFW window" << std::endl;
@@ -23,9 +39,34 @@ int main() {
 		return -1;
 	}
 
-	glfwMakeContextCurrent(window);
-	std::cin.get();
 
+
+	glfwMakeContextCurrent(window);
+
+	// OS-specific loading proc
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to init GLAD" << std::endl;
+		return -1;
+	}
+	// set the size of rendering window
+	// lower left corner, width, height
+	// translate -1, 1 to 0,800 and 0,600
+	glViewport(0, 0, 800, 600); 
+
+
+	//glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	
+
+	// rendering loop
+
+	while (!glfwWindowShouldClose(window)) {  // check close
+		glfwSwapBuffers(window);
+		glfwPollEvents();   // handle keyboard and 
+	}
+
+	// release all the resource before exit
+	glfwTerminate();
+	
 	return 0;
 }
 
