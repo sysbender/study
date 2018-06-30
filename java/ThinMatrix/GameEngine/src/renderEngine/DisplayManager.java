cc55eb45669870fd412171775e1b647e64e0aa79
sysbender <sysbender@gmail.com>
2018-06-30 11:02:26 -0400

    lwjgl - first display

---
+++ java/ThinMatrix/GameEngine/src/renderEngine/DisplayManager.java
@@ -0,0 +1,55 @@
+package renderEngine;
+
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.PixelFormat;
+ 
+
+
+
+public class DisplayManager {
+	private static final int WIDTH=800;	
+	private static final int HEIGHT =600;
+	private static final int FPS =120;
+	
+
+	
+	public static void createDisplay() {
+		
+		ContextAttribs attribs = new ContextAttribs(3,2); //version 
+		
+		attribs.withForwardCompatible(true);
+		attribs.withProfileCore(true);
+		
+		try {
+			Display.setDisplayMode(new DisplayMode( WIDTH, HEIGHT));
+			
+			Display.create(new PixelFormat(), attribs);
+			
+			Display.setTitle("our first display");
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		GL11.glViewport(0, 0, WIDTH, HEIGHT);
+		
+	}
+	
+	
+	public static void updateDisplay() {
+		Display.sync(FPS);
+		Display.update();
+	}
+	
+	
+	public static void closeDisplay() {
+		Display.destroy();
+		
+	}
+
+}
